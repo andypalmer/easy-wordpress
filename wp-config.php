56079a8614fddb8eecab7b1098387985d0631fd3
Andy Palmer <andy@andypalmer.com>
2014-06-24 14:13:05 +0100

    install s3-content plugin, default theme (twentyfourteen) and wordpress core; get configuration from environment

---
+++ wp-config.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+/** Settings for composer-ised plugins and themes */
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
+
+/** Absolute path to the composer-ised WordPress directory. */
+if ( !defined('ABSPATH') )
+  define('ABSPATH', dirname(__FILE__) . '/wp/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
+// ** Heroku database settings - from Heroku Environment ** //
+$db = parse_url($_ENV["DATABASE_URL"]);
+define('DB_NAME', trim($db["path"],"/"));
+define('DB_USER', $db["user"]);
+define('DB_PASSWORD', $db["pass"]);
+define('DB_HOST', $db["host"]);
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+unset($db);
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+$vars = array(
+  'AUTH_KEY',
+  'SECURE_AUTH_KEY',
+  'NONCE_KEY',
+  'NONCE_SALT',
+  'AUTH_SALT',
+  'SECURE_AUTH_SALT',
+  'LOGGED_IN_KEY',
+  'LOGGED_IN_SALT'
+);
+foreach ($vars as $var) {
+    define($var, getenv($var));
+}
+unset($vars);
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = getenv('TABLE_PREFIX') ?: 'wp_';
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', getenv('WPLANG') ?: '');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', getenv('WP_DEBUG'));
+
+/* That's all, stop editing! Happy blogging. */
+
